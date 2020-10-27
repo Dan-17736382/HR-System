921dee86c43412f49a4a65413c48dc2d06a39e5d
Jack <18029949@students.southwales.ac.uk>
2020-10-27 13:19:24 +0000

    Setup sqlite3 with the application
    Altered initial code to be better encapsulated, begun to incorporate sqlite3. The Application class now creates and is responsible for a database connection. Removed exit button, as a better alternative was to override the behaviour of the default close button.

--- gui.py
+++ gui.py
@@ -1,23 +1,44 @@
 from tkinter import *
 from tkinter import ttk
-
-window = Tk()
+import sqlite3
 
 class Application:
-    def __init__(self, master):
-        button = ttk.Button(master, text = "Exit", command = self.exit)
-        button.place(x=700, y=550)
+    def __init__(self):
+        # create a TK instance
+        self.window = Tk()
 
-    def exit(root):
-        window.destroy()
+        # establish a database connection
+        self.connection = sqlite3.connect("user_database.db")
+        self.cursor = self.connection.cursor()
+
+        # override the behaviour of the close button so that we make sure
+        # to close the database connection on program exit
+        self.window.protocol('WM_DELETE_WINDOW', self.exit)
+
+    def exit(self):
+        # make sure to close the database connection on program exit
+        self.connection.commit()
+        self.connection.close()
+
+        self.window.destroy()
 
 
-def main(): 
-    menu = Application(window)
-    window.geometry("800x600")
-    window.title("Test Window")
-    window.resizable(width=False, height=False)
-    window.mainloop()
+def main():
+    # create an instance of the Application class
+    application = Application()
+
+    # set initial window size to 800x600 and centre the window
+    initial_width = 800
+    initial_height = 600
+    x = (application.window.winfo_screenwidth() / 2) - (initial_width / 2)
+    y = (application.window.winfo_screenheight() / 2) - (initial_height / 2)
+    application.window.geometry('%dx%d+%d+%d' % (initial_width, initial_height, x, y))
+
+    # set the window's title
+    application.window.title("Test Window")
+
+    # start the application
+    application.window.mainloop()
 
 
 if __name__ == "__main__": main()
